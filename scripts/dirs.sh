05bed091993b41463639c96ec4d32bf7b110e1d2
Anders L. Hurum <anders@sensario.com>
2018-05-15 13:18:47 +0200

    feat: added script for creating default working dirs

---
+++ scripts/dirs.sh
@@ -0,0 +1,8 @@
+echo "Creating folder structure for filesystem ... "
+mkdir -p ~/Documents/Old
+mkdir -p ~/Documents/LaTeX
+mkdir -p ~/Documents/Articles
+mkdir -p ~/Documents/Work
+mkdir -p ~/Documents/Embedded
+mkdir -p ~/Documents/Modelling
+echo "Done!"
