9f63edce89782b4ea53351e4166d833329ca185d
Anders L. Hurum <andershurum@gmail.com>
2018-09-26 15:46:38 +0200

    cleared the scripts directory

--- .scripts/dirs.sh
+++
@@ -1,8 +0,0 @@
-echo "Creating folder structure for filesystem ... "
-mkdir -p ~/Documents/Old
-mkdir -p ~/Documents/LaTeX
-mkdir -p ~/Documents/Articles
-mkdir -p ~/Documents/Work
-mkdir -p ~/Documents/Embedded
-mkdir -p ~/Documents/Modelling
-echo "Done!"
